e74b68fa029fd4045a26c7f73eeaad3e3878a50c
Marcos Vinicius <marcos.v.silva@live.com>
2019-10-23 12:39:55 -0300

    Refactoring

--- else/algo.h
+++ else/algo.h
@@ -15,18 +15,13 @@ namespace ELSE{
 		Mat pic=Mat::zeros(input_img.rows/rz_fakk, input_img.cols/rz_fakk, CV_8UC1);
 		resize(input_img, pic,pic.size());
 
-
 		normalize(pic, pic, 0, 255, NORM_MINMAX, CV_8U);
 
-
-
 		double border=0.1;
-
 
 		RotatedRect ellipse;
 
 		ellipse=blob_finder(&pic,border);
-
 
 		ellipse.size.height=ellipse.size.height*rz_fakk;
 		ellipse.size.width=ellipse.size.width*rz_fakk;
@@ -34,11 +29,7 @@ namespace ELSE{
 		ellipse.center.x=ellipse.center.x*rz_fakk;
 		ellipse.center.y=ellipse.center.y*rz_fakk;
 
-
 		return ellipse;
-	
-
-
 	
 	}
 }
