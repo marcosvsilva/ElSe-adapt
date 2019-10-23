cec978551d0dbba938695e4df544a3b23640d136
Marcos Vinicius <marcos.v.silva@live.com>
2019-10-22 23:51:15 -0300

    Formats code

--- else/algo.h
+++ else/algo.h
@@ -1,61 +1,42 @@
-/*
-
-  The Code is created based on the method from the paper:
-  "Evaluation of State-of-the-Art Pupil Detection Algorithms on Remote Eye Images", W. Fuhl, D. Geisler, T. Santini, E. Kasneci
-  ACM International Joint Conference on Pervasive and Ubiquitous Computing: Adjunct publication -- PETMEI 2016
- 
-
-*/
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/core/core.hpp>
 
-
 #include "blob_gen.h"
-
 
 namespace ELSE{
 
+	static cv::RotatedRect run(cv::Mat input_img){
 
-static cv::RotatedRect run(cv::Mat input_img){
+		float rz_fakk=float(input_img.cols)/384.0;
 
-
-	float rz_fakk=float(input_img.cols)/384.0;
-
-	cv::Mat pic=cv::Mat::zeros(input_img.rows/rz_fakk, input_img.cols/rz_fakk, CV_8UC1);
-	cv::resize(input_img, pic,pic.size());
+		cv::Mat pic=cv::Mat::zeros(input_img.rows/rz_fakk, input_img.cols/rz_fakk, CV_8UC1);
+		cv::resize(input_img, pic,pic.size());
 
 
-	cv::normalize(pic, pic, 0, 255, cv::NORM_MINMAX, CV_8U);
+		cv::normalize(pic, pic, 0, 255, cv::NORM_MINMAX, CV_8U);
 
 
 
-	double border=0.1;
+		double border=0.1;
 
 
-	cv::RotatedRect ellipse;
+		cv::RotatedRect ellipse;
 
-	ellipse=blob_finder(&pic,border);
-
-
-	ellipse.size.height=ellipse.size.height*rz_fakk;
-	ellipse.size.width=ellipse.size.width*rz_fakk;
-
-	ellipse.center.x=ellipse.center.x*rz_fakk;
-	ellipse.center.y=ellipse.center.y*rz_fakk;
+		ellipse=blob_finder(&pic,border);
 
 
-	return ellipse;
+		ellipse.size.height=ellipse.size.height*rz_fakk;
+		ellipse.size.width=ellipse.size.width*rz_fakk;
+
+		ellipse.center.x=ellipse.center.x*rz_fakk;
+		ellipse.center.y=ellipse.center.y*rz_fakk;
+
+
+		return ellipse;
 	
 
 
 	
+	}
 }
-
-
-
-
-}
-
-
-
